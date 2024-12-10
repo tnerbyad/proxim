cb3c47b33600eccc6a99f68a34cbaca1a33cd052
tnerbyad <hoofinit@gmail.com>
2024-12-09 22:21:52 -0700

    changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 import os
 from geopy.distance import geodesic  # Install via 'pip install geopy'
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 app.secret_key = "your_secret_key_here"  # Replace with a secure key
 
 @app.route("/")
