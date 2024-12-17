53127204c78f70bc57e916cee68d95f4f7f59a45
tnerbyad <hoofinit@gmail.com>
2024-12-16 20:12:26 -0700

    changes

--- static/js/script.js
+++ static/js/script.js
@@ -105,7 +105,7 @@ function calculateDirectionOffset(bearing, alpha){
     //bearing is the direction from current location to target location
     //alpha is the direction the phone is pointing
     if (!isNaN(bearing) && !isNaN(alpha)) {
-        return (360-(alpha - bearing));
+        return (bearing - alpha + 360);
     } else if (!isNaN(bearing)) {
         return(bearing);
     } else if (!isNaN(alpha)) {
